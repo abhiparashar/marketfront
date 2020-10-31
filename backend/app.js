d84352b50ad2364f008294be1f976f6b92aa562a
abhi parashar <aparashar103@gmail.com>
2020-10-31 18:46:57 +0530

    axios API implementation

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require('cors')
 dotenv.config()
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const connectDB = require('./config/db')
 
 const authRoute = require('./routes/auth')
-const userRoute = require('./routes/auth')
+const userRoute = require('./routes/user')
+
 
 
 const app = express()
@@ -19,7 +21,8 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use('/api/v1/auth',authRoute)
-app.use('/api/v1/auth',userRoute)
+app.use('/api/v1/user',userRoute)
+
 
 
 const port = process.env.PORT || 7500
@@ -27,3 +30,6 @@ const port = process.env.PORT || 7500
 app.listen(port,()=>{
     console.log(`server is listening at port ${port}`)
 })
+
+
+
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,14 +1,39 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
 
 const{requireSign,isAuth,isAdmin} = require('../controllers/auth')
 const{userById} = require('../controllers/user')
 
-router.get('/secret/:userId',requireSign,isAuth,isAdmin,(req,res)=>{
-    res.status(200).send({
-        user:req.body
-    })
-})
+// router.get('/secret/:userId',requireSign,isAuth,isAdmin,(req,res)=>{
+//     res.status(200).send({
+//         user:req.body
+//     })
+// })
+
 router.param("userId",userById)
+
+router.get('/getdata',async(req,res)=>{
+        axios.get(
+    'https://books.zoho.com/api/v3/organizations?organization_id=10234695',
+    {headers: {
+            "Access-Control-Allow-Origin" : "*",
+            "Content-type": "Application/json",
+            "Authorization":  'db36e02a50b57e081efe533a8a0f834b'
+            }   
+        }
+  )
+  .then((response) => {
+       let data = response.data;
+       res.send(
+           data
+       )
+    },
+    (error) => {
+      var status = error.response.status
+      res.send(error)
+    }
+  );
+})
 
 module.exports = router
